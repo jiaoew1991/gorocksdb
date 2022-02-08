47bf48d07a63118665d482f0d0c89a64a70b664d
Nemo <69466447+soothing-rain@users.noreply.github.com>
2022-02-08 15:29:18 +0800

    Update gorocksdb to support Rocksdb 6.27.3 (#1)
    * Updating the README with proposed versioning scheme
    
    - in order to manage breaking changes in v6  of rocksdb I am proposing create a v1.0 tag for 5.16+ and moving master (and all v2.0+ tags) on to allow breaking changes only found in rocksdb 6
    
    * Support Rocksdb 6.27.3
    
    * Explicit pass and ignore two of rocksdb C errors.
    
    Co-authored-by: bibby <james.bibby@datadoghq.com>

--- db.go
+++ db.go
@@ -606,6 +606,9 @@ func (db *DB) GetApproximateSizes(ranges []Range) []uint64 {
 		}
 	}()
 
+    var cErr *C.char
+    // Explicitly ignoring error.
+    // TODO: Handle the error properly.
 	C.rocksdb_approximate_sizes(
 		db.c,
 		C.int(len(ranges)),
@@ -613,7 +616,8 @@ func (db *DB) GetApproximateSizes(ranges []Range) []uint64 {
 		&cStartLens[0],
 		&cLimits[0],
 		&cLimitLens[0],
-		(*C.uint64_t)(&sizes[0]))
+		(*C.uint64_t)(&sizes[0]),
+		&cErr)
 
 	return sizes
 }
@@ -647,6 +651,9 @@ func (db *DB) GetApproximateSizesCF(cf *ColumnFamilyHandle, ranges []Range) []ui
 		}
 	}()
 
+    var cErr *C.char
+    // Explicitly ignoring error.
+    // TODO: Handle the error properly.
 	C.rocksdb_approximate_sizes_cf(
 		db.c,
 		cf.c,
@@ -655,7 +662,8 @@ func (db *DB) GetApproximateSizesCF(cf *ColumnFamilyHandle, ranges []Range) []ui
 		&cStartLens[0],
 		&cLimits[0],
 		&cLimitLens[0],
-		(*C.uint64_t)(&sizes[0]))
+		(*C.uint64_t)(&sizes[0]),
+		&cErr)
 
 	return sizes
 }
